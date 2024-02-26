2f4ff4287b5760ad01bde4b8799a80dcc3e86513
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-26 16:22:58 +0530

    Add project files.

---
+++ librarymanagementsystem/AddUserForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace librarymanagementsystem
+{
+    public partial class AddUserForm : Form
+    {
+        public AddUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUserForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+    }
+}
---
+++ librarymanagementsystem/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace librarymanagementsystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of Form2
+            AddUserForm addUserForm = new AddUserForm();
+
+            // Show Form2
+            addUserForm.Show();
+        }
+    }
+}
