2bfcf02b9bee5034884f5918b7bf1ddd37c3132d
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-26 18:35:09 +0530

    db connection configured

---
+++ librarymanagementsystem/AppDb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librarymanagementsystem
+{
+    public class AppDb
+    {
+        //connection
+        SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=librarysystem;Integrated Security=True;Encrypt=False");
+
+
+        //create function to get connection
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+
+        //create function to open connection
+        public void openConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+
+        //create function to close connection
+        public void closeConnection()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
