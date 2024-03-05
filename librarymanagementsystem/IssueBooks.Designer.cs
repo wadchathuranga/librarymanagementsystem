842f625beea70d16e6dafa97a39d8dab0df62bb2
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-05 13:06:13 +0530

    ui design changed

--- librarymanagementsystem/IssueBooks.Designer.cs
+++ librarymanagementsystem/IssueBooks.Designer.cs
@@ -33,8 +33,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.userIdTextBox = new System.Windows.Forms.TextBox();
             this.bookIdTextBox = new System.Windows.Forms.TextBox();
             this.issueDateLabel = new System.Windows.Forms.Label();
@@ -74,82 +72,64 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 71);
+            this.label2.Location = new System.Drawing.Point(24, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.Size = new System.Drawing.Size(68, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "UserId";
+            this.label2.Text = "User Id:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(24, 194);
+            this.label3.Location = new System.Drawing.Point(420, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.Size = new System.Drawing.Size(70, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "BookId";
+            this.label3.Text = "Book Id:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(24, 331);
+            this.label4.Location = new System.Drawing.Point(420, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.Size = new System.Drawing.Size(95, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Issue Date";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Text = "Issue Date:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(24, 359);
+            this.label5.Location = new System.Drawing.Point(611, 202);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.Size = new System.Drawing.Size(86, 20);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Due Date";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(413, 400);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 20);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Title";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(563, 400);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 16);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Status";
+            this.label5.Text = "Due Date:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // userIdTextBox
             // 
-            this.userIdTextBox.Location = new System.Drawing.Point(144, 71);
+            this.userIdTextBox.Location = new System.Drawing.Point(115, 69);
             this.userIdTextBox.Name = "userIdTextBox";
-            this.userIdTextBox.Size = new System.Drawing.Size(100, 22);
+            this.userIdTextBox.Size = new System.Drawing.Size(134, 22);
             this.userIdTextBox.TabIndex = 7;
             // 
             // bookIdTextBox
             // 
-            this.bookIdTextBox.Location = new System.Drawing.Point(144, 194);
+            this.bookIdTextBox.Location = new System.Drawing.Point(514, 70);
             this.bookIdTextBox.Name = "bookIdTextBox";
-            this.bookIdTextBox.Size = new System.Drawing.Size(100, 22);
+            this.bookIdTextBox.Size = new System.Drawing.Size(136, 22);
             this.bookIdTextBox.TabIndex = 8;
             // 
             // issueDateLabel
             // 
             this.issueDateLabel.AutoSize = true;
-            this.issueDateLabel.Location = new System.Drawing.Point(141, 334);
+            this.issueDateLabel.Location = new System.Drawing.Point(521, 205);
             this.issueDateLabel.Name = "issueDateLabel";
             this.issueDateLabel.Size = new System.Drawing.Size(34, 16);
             this.issueDateLabel.TabIndex = 9;
@@ -158,7 +138,7 @@
             // dueDateLabel
             // 
             this.dueDateLabel.AutoSize = true;
-            this.dueDateLabel.Location = new System.Drawing.Point(141, 362);
+            this.dueDateLabel.Location = new System.Drawing.Point(703, 206);
             this.dueDateLabel.Name = "dueDateLabel";
             this.dueDateLabel.Size = new System.Drawing.Size(34, 16);
             this.dueDateLabel.TabIndex = 10;
@@ -166,9 +146,9 @@
             // 
             // findUserButton
             // 
-            this.findUserButton.Location = new System.Drawing.Point(250, 71);
+            this.findUserButton.Location = new System.Drawing.Point(256, 67);
             this.findUserButton.Name = "findUserButton";
-            this.findUserButton.Size = new System.Drawing.Size(75, 23);
+            this.findUserButton.Size = new System.Drawing.Size(103, 23);
             this.findUserButton.TabIndex = 11;
             this.findUserButton.Text = "Find";
             this.findUserButton.UseVisualStyleBackColor = true;
@@ -177,11 +157,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(390, 113);
+            this.dataGridView1.Location = new System.Drawing.Point(28, 289);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(402, 149);
+            this.dataGridView1.Size = new System.Drawing.Size(748, 149);
             this.dataGridView1.TabIndex = 15;
             // 
             // bookStatusLabel
@@ -196,9 +176,10 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(516, 87);
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(308, 266);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(160, 16);
+            this.label14.Size = new System.Drawing.Size(202, 20);
             this.label14.TabIndex = 17;
             this.label14.Text = "User\'s Loaned Out Books";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -252,18 +233,18 @@
             this.groupBox1.Controls.Add(this.typeLabel);
             this.groupBox1.Controls.Add(this.nicLabel);
             this.groupBox1.Controls.Add(this.userNameLabel);
-            this.groupBox1.Location = new System.Drawing.Point(28, 105);
+            this.groupBox1.Location = new System.Drawing.Point(28, 101);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(297, 59);
+            this.groupBox1.Size = new System.Drawing.Size(331, 59);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "User Info";
             // 
             // bookFindButton
             // 
-            this.bookFindButton.Location = new System.Drawing.Point(250, 194);
+            this.bookFindButton.Location = new System.Drawing.Point(656, 69);
             this.bookFindButton.Name = "bookFindButton";
-            this.bookFindButton.Size = new System.Drawing.Size(75, 23);
+            this.bookFindButton.Size = new System.Drawing.Size(120, 23);
             this.bookFindButton.TabIndex = 21;
             this.bookFindButton.Text = "Find";
             this.bookFindButton.UseVisualStyleBackColor = true;
@@ -271,9 +252,9 @@
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(28, 399);
+            this.backButton.Location = new System.Drawing.Point(28, 187);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(128, 39);
+            this.backButton.Size = new System.Drawing.Size(147, 55);
             this.backButton.TabIndex = 23;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = true;
@@ -281,9 +262,9 @@
             // 
             // bookIssueButton
             // 
-            this.bookIssueButton.Location = new System.Drawing.Point(162, 399);
+            this.bookIssueButton.Location = new System.Drawing.Point(181, 187);
             this.bookIssueButton.Name = "bookIssueButton";
-            this.bookIssueButton.Size = new System.Drawing.Size(163, 39);
+            this.bookIssueButton.Size = new System.Drawing.Size(178, 55);
             this.bookIssueButton.TabIndex = 24;
             this.bookIssueButton.Text = "Issue";
             this.bookIssueButton.UseVisualStyleBackColor = true;
@@ -293,9 +274,9 @@
             // 
             this.groupBox2.Controls.Add(this.bookTitleLabel);
             this.groupBox2.Controls.Add(this.bookStatusLabel);
-            this.groupBox2.Location = new System.Drawing.Point(28, 225);
+            this.groupBox2.Location = new System.Drawing.Point(424, 101);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(297, 87);
+            this.groupBox2.Size = new System.Drawing.Size(352, 87);
             this.groupBox2.TabIndex = 25;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Book Info";
@@ -318,8 +299,6 @@
             this.Controls.Add(this.dueDateLabel);
             this.Controls.Add(this.issueDateLabel);
             this.Controls.Add(this.bookIdTextBox);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -345,8 +324,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox userIdTextBox;
         private System.Windows.Forms.TextBox bookIdTextBox;
         private System.Windows.Forms.Label issueDateLabel;
