502d565e87f23084eb7d7b395111a22bd0225d7f
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-05 22:30:40 +0530

    return books form created

---
+++ librarymanagementsystem/ReturnBooks.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace librarymanagementsystem
+{
+    partial class ReturnBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.nicLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.userIdTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.findUserButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.typeLabel);
+            this.groupBox1.Controls.Add(this.nicLabel);
+            this.groupBox1.Controls.Add(this.userNameLabel);
+            this.groupBox1.Location = new System.Drawing.Point(37, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 59);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Info";
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(264, 26);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(33, 16);
+            this.typeLabel.TabIndex = 20;
+            this.typeLabel.Text = "type";
+            this.typeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nicLabel
+            // 
+            this.nicLabel.AutoSize = true;
+            this.nicLabel.Location = new System.Drawing.Point(134, 26);
+            this.nicLabel.Name = "nicLabel";
+            this.nicLabel.Size = new System.Drawing.Size(45, 16);
+            this.nicLabel.TabIndex = 19;
+            this.nicLabel.Text = "nic no.";
+            this.nicLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(24, 26);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(67, 16);
+            this.userNameLabel.TabIndex = 18;
+            this.userNameLabel.Text = "username";
+            this.userNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // userIdTextBox
+            // 
+            this.userIdTextBox.Location = new System.Drawing.Point(124, 84);
+            this.userIdTextBox.Name = "userIdTextBox";
+            this.userIdTextBox.Size = new System.Drawing.Size(134, 22);
+            this.userIdTextBox.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "User Id:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // findUserButton
+            // 
+            this.findUserButton.Location = new System.Drawing.Point(264, 82);
+            this.findUserButton.Name = "findUserButton";
+            this.findUserButton.Size = new System.Drawing.Size(103, 28);
+            this.findUserButton.TabIndex = 25;
+            this.findUserButton.Text = "Find";
+            this.findUserButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(324, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Return Books";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ReturnBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.userIdTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.findUserButton);
+            this.Name = "ReturnBooks";
+            this.Text = "ReturnBooks";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label nicLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox userIdTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button findUserButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
